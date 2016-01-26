ef35b1309a913c19833e8bca04767b7f98076d9e
Kristian Bjørnard <kristian.bjornard@gmail.com>
2016-01-26 10:38:27 -0500

    okay, so this is a template for one plan?

--- src/components/Plan/Plans.js
+++ src/components/Plan/Plans.js
@@ -3,9 +3,19 @@ import React, { PropTypes } from 'react'
 // Basic suggestion button.
 function Plans({ message }) {
   return (
-    <h2 className="loading">
-      { message }
-    </h2>
+    <div className="six columns">
+      <div className="inner bg-blue white clearfix">
+        <div className="twelve columns">
+          <h2 className="mt1">{ membership level }</h2>
+
+          <OptionsBox />
+        </div>
+        <div className="text-center">
+          <div className="six columns mt1 mb0 p1 bg-orange white">Join</div>
+          <div className="six columns mt1 mb0 p1 bg-lightgreen white">Gift</div>
+        </div>
+      </div>
+    </div>
   )
 }
 

f651d95c4f3acf5fa1e0f85dd3657396f13bc60a
Kai Curry <kai@cape.io>
2016-01-26 14:07:47 -0600

    passing plans prop

--- src/containers/SelectPlan.js
+++ src/containers/SelectPlan.js
@@ -1,12 +1,27 @@
 import { connect } from 'react-redux'
+import values from 'lodash/values'
 import Component from '../components/Support/Support'
 // Redux connections.
 
 function mapStateToProps(state) {
-  const { entity: { support } } = state
-
+  const { entity: { plan } } = state
+  const plans = []
+  if (plan.individual) {
+    plans.push({
+      type: 'individual',
+      title: 'Individual Membership',
+      plans: values(plan.individual),
+    })
+  }
+  if (plan.team) {
+    plans.push({
+      type: 'team',
+      title: 'Team Membership',
+      plans: values(plan.individual),
+    })
+  }
   return {
-    support,
+    plans,
   }
 }
 // const mapDispatchToProps = {

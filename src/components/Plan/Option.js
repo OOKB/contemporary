2050c41400db52788341ab2fc5f0dfc7b21e8df7
Kai Curry <kai@cape.io>
2016-01-26 15:31:39 -0600

    cleanup

--- src/components/Plan/Option.js
+++ src/components/Plan/Option.js
@@ -1,14 +1,14 @@
 import React, { PropTypes } from 'react'
 
 // Basic suggestion button.
-function Option({ name, amount }) {
+function Option({ name, amount, id }) {
   const price = `$${amount / 100}`
   return (
-    <div className="input-group">
-      <label htmlFor="{ name }">{ name }</label>
-      <input type="radio" name="{ name }" id="{ name }" defaultvalue="{ price }" />
+    <label>
+      <input type="radio" id={ id } value={ id } />
+      <span className="name">{ name }</span>
       <span className="price">{ price }</span>
-    </div>
+    </label>
   )
 }
 

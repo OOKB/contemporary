71b88eb9cedf60bfd14dab8275c2f59f1e41ffc0
Kristian Bjørnard <kristian.bjornard@gmail.com>
2016-01-26 11:29:15 -0500

    link out to contemporary CSS files so things start to look like something

--- src/components/Plan/OptionsBox.js
+++ src/components/Plan/OptionsBox.js
@@ -4,19 +4,14 @@ import Option from './Option'
 // Basic suggestion button.
 function OptionsBox({ name, description, options }) {
   return (
-    <div>
-      <h3 className="mt1">{name}</h3>
+    <div className="six columns">
+      <h3 className="mt1">A Name {name}</h3>
       { description && <div>{description}</div>}
       { options && options.map(option => (
         <Option {...option} />
       ))
       }
-      <Submit
-        text={submit}
-        handleSubmit={handleSubmit}
-        invalid={invalid}
-        pristine={pristine}
-      />
+      <input type="submit" name="join" defaultValue="Join" />
     </div>
   )
 }
--- src/components/Plan/Plans.js
+++ src/components/Plan/Plans.js
@@ -4,7 +4,8 @@ import OptionsBox from './OptionsBox'
 // Basic suggestion button.
 function Plans({ message, membershipLevel }) {
   return (
-    <div className="six columns">
+    <div>
+      <OptionsBox />
       <OptionsBox />
       {/* so, to print more than one box, this needs to be some sort of map? */}
     </div>

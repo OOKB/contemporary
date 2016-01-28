58b34d07f2d872ceb1e06a58115f09e369a7375b
Kai Curry <kai@cape.io>
2016-01-28 16:58:54 -0600

    rm amp - style

--- src/components/Editable/input/Input.js
+++ src/components/Editable/input/Input.js
@@ -1,5 +1,5 @@
-import React, { Component, PropTypes } from 'react';
-import Icon from '../Icon';
+import React, { Component, PropTypes } from 'react'
+import Icon from '../Icon'
 
 // Simple wrapper around an input field.
 // 1. Checks for changes every 300ms. Useful for safari autocomplete.
@@ -9,73 +9,73 @@ class Input extends Component {
 
   componentDidMount() {
     // When initialized trigger the tick function every interval.
-    this.interval = setInterval(() => this.tick(), 300);
+    this.interval = setInterval(() => this.tick(), 300)
   }
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   // Only update when the props value changes.
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.value !== this.props.value;
+    return nextProps.value !== this.props.value
   }
 
   // The value has changed. Do stuff.
   changeValue(newValue) {
-    const {value, onChange} = this.props;
+    const {value, onChange} = this.props
     // When it's a new value send it to parent.
     if (newValue !== value) {
-      onChange(newValue);
+      onChange(newValue)
     }
   }
 
   // Extract new value from change event.
   handleChange(e) {
-    const newValue = e.target.value;
+    const newValue = e.target.value
     // Pass it along.
-    this.changeValue(newValue);
+    this.changeValue(newValue)
   }
   handleBlur(e) {
-    const newValue = e.target.value;
-    const {onClose} = this.props;
+    const newValue = e.target.value
+    const {onClose} = this.props
     // Save value.
     if (newValue) {
       // submit.
     }
     // Empty value. Just close the form.
     else {
-      onClose();
+      onClose()
     }
   }
   // The function that is called every interval.
   tick() {
-    const {id} = this.props;
+    const {id} = this.props
     // Get the form field value. Not using refs so it's usable with Redux...
-    // const fieldVal = this.refs[id].getDOMNode().value;
+    // const fieldVal = this.refs[id].getDOMNode().value
     const fieldVal = document.getElementById(id).value
     // If the field has a value send it off.
     if (fieldVal) {
-      this.changeValue(fieldVal);
+      this.changeValue(fieldVal)
     }
   }
   clearInputValue() {
-    //console.log('clear');
-    this.changeValue('');
+    //console.log('clear')
+    this.changeValue('')
   }
   handleKeyDown(e) {
-    const {onClose, onNext} = this.props;
+    const {onClose, onNext} = this.props
     switch (e.keyCode) {
       // escape key.
       case 27:
         if (onClose) {
-          e.preventDefault();
-          onClose();
+          e.preventDefault()
+          onClose()
         }
-        break;
+        break
       case 9:
         if (onNext) {
-          e.preventDefault();
-          onNext();
+          e.preventDefault()
+          onNext()
         }
       default:
     }
@@ -83,7 +83,7 @@ class Input extends Component {
     // tab 9
   }
   render() {
-    const {id, onChange, value, ...other} = this.props;
+    const {id, onChange, value, ...other} = this.props
     let clearEl = false
     if (value) {
       clearEl =
@@ -112,7 +112,7 @@ class Input extends Component {
         />
         {clearEl}
       </div>
-    );
+    )
   }
 }
 
@@ -123,4 +123,4 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   value: PropTypes.string
 }
-export default Input;
+export default Input

2f42b148407d15dedce602aad18e4d47c845019d
Kai Curry <kai@cape.io>
2016-01-29 13:43:20 -0600

    It's this easy.

--- src/components/Editable/PreviewTextEditable.js
+++ src/components/Editable/PreviewTextEditable.js
@@ -1,13 +1,31 @@
 import React, { PropTypes } from 'react'
-
+import radium from 'radium'
 // The button that gets clicked for simple editable text fields.
 
-function PreviewTextEditable({ className, emptyText, title, value, onClick }) {
+const styles = {
+  base: {
+    background: 'transparent',
+    backgroundColor: 'none',
+    backgroundImage: 'none',
+    border: 'none',
+    textDecoration: 'none',
+    borderBottomStyle: 'dashed',
+    borderBottomWidth: '1px',
+    // borderBottomColor: currentColor,
+    borderRadius: 0,
+    padding: 0,
+    paddingTop: '.5em',
+    paddingBottom: '.1em',
+  },
+}
+
+function PreviewTextEditable({ className, emptyText, title, value, onClick, style }) {
   return (
     <button
       className={ className }
       onClick={ onClick }
       title={ title }
+      style={[ styles.base, style ]}
     >
       { value || emptyText }
     </button>
@@ -22,8 +40,9 @@ PreviewTextEditable.propTypes = {
   className: PropTypes.string,
   emptyText: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  style: PropTypes.object,
   title: PropTypes.string,
   value: PropTypes.string.isRequired,
 }
 
-export default PreviewTextEditable
+export default radium(PreviewTextEditable)

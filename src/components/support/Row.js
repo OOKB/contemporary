ebe9701680faac82eef723bda7fa1306f7094c9f
Kai Curry <kai@cape.io>
2016-01-31 19:37:49 -0600

    remove blurb from row

--- src/components/support/Row.js
+++ src/components/support/Row.js
@@ -1,17 +1,12 @@
 import React, { PropTypes } from 'react'
 
 // Basic suggestion button.
-function Row({ children, sectionName, sectionBlurb }) {
+function Row({ children, sectionName }) {
   return (
       <article className="pad-top border-top">
         <section id={ sectionName }>
           <h2>{ sectionName }</h2>
-          <div className="group mb4">
-            <div className="six columns">
-              <p>{ sectionBlurb }</p>
-              { children }
-            </div>
-          </div>
+          { children }
         </section>
       </article>
   )
@@ -20,7 +15,6 @@ function Row({ children, sectionName, sectionBlurb }) {
 Row.propTypes = {
   children: PropTypes.node,
   sectionName: PropTypes.string.isRequired,
-  sectionBlurb: PropTypes.string.isRequired,
 }
 
 export default Row

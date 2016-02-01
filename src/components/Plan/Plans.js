15e6ec4bedf6a0a7b16f71050b31225112a60ccc
Kai Curry <kai@cape.io>
2016-02-01 17:03:35 -0600

    show message on subscribed

--- src/components/Plan/Plans.js
+++ src/components/Plan/Plans.js
@@ -1,13 +1,17 @@
 import React, { PropTypes, Component } from 'react'
 import OptionsBox from './OptionsBox'
 import Subscribing from './Subscribing'
+import Subscribed from './Subscribed'
 
 class Plans extends Component {
   render() {
     const { plans, stripeAction, stripeState } = this.props
-    const { amount, description, subscribing } = stripeState
+    const { amount, description, subscribing, subscribed } = stripeState
     if (subscribing) {
       return <Subscribing amount={amount} description={description} />
+    }
+    if (subscribed) {
+      return <Subscribed amount={amount} description={description} />
     }
     return (
       <div className="six columns">

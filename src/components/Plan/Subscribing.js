6784942d91443dc9d9bbbea6d864c082f0040316
Kristian Bjørnard <kristian.bjornard@gmail.com>
2016-02-04 18:20:36 -0500

    styling subscribing block and text

--- src/components/Plan/Subscribing.js
+++ src/components/Plan/Subscribing.js
@@ -4,9 +4,11 @@ import React, { PropTypes } from 'react'
 function Subscribing({ amount, description, statusMsg }) {
   const title = `${description}: $${amount / 100}`
   return (
-    <div className="subscribing six columns">
-      <h3>{ title }</h3>
-      <p>{ statusMsg }</p>
+    <div className="subscribing three columns bg-orange">
+      <div className="inner white pt1">
+        <h3>{ title }</h3>
+        <p>{ statusMsg }</p>
+      </div>
     </div>
   )
 }

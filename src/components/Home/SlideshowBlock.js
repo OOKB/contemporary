a7b75799c2ca87cfbd270775970ce0d3fd349b9f
Kai Curry <kai@cape.io>
2016-02-04 20:24:00 -0600

    Let me fix this for you

--- src/components/Home/SlideshowBlock.js
+++ src/components/Home/SlideshowBlock.js
@@ -1,11 +1,17 @@
 import React, { PropTypes } from 'react'
 
 function SlideshowBlock({  }) {
+  const backgroundImage = 'http://contemporary.ookb.co/img/ghostfood.jpg'
   return (
     <div className="nine columns">
       <div id="slideshow-wrap" className="cycle-slideshow">
         <div className="item">
-          <div className="inner tile imged bg-black" style="background-image:url('http://contemporary.ookb.co/img/ghostfood.jpg');">
+          <div
+            className="inner tile imged bg-black"
+            style={{
+              backgroundImage: `url(${backgroundImage})`,
+            }}
+          >
             <a href="/projects/ghostfood/" className="bigBlock">
               <h1>GhostFood</h1>
               <p className="blurb"></p>
@@ -14,8 +20,16 @@ function SlideshowBlock({  }) {
         </div>
       </div>
       <div className="controls">
-        <div className="prevControl"> <i className="fa fa-angle-left fa-4x"> <span className="hidden">Left/Previous</span> </i> </div>
-        <div className="nextControl"> <i className="fa fa-angle-right fa-4x"> <span className="hidden">Right/Next</span> </i> </div>
+        <div className="prevControl">
+          <i className="fa fa-angle-left fa-4x">
+            <span className="hidden">Left/Previous</span>
+          </i>
+        </div>
+        <div className="nextControl">
+          <i className="fa fa-angle-right fa-4x">
+            <span className="hidden">Right/Next</span>
+          </i>
+        </div>
       </div>
     </div>
   )
